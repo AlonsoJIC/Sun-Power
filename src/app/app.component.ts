ca68f3e1eedbdbf207beae8736d01c7f1083d5b3
AlonsoJIC <jalonsojic@gmail.com>
2023-01-09 04:03:39 -0600

    3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,56 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyectoAngular';
+  products = [
+    {
+      name: 'angular',
+      price: 565,
+      image: './assets/images/angular.png',
+    },
+    {
+      name: 'bootstrap',
+      price: 356,
+      image: './assets/images/bootstrap.png'
+    },
+    {
+      name: 'css',
+      price: 34,
+      image: './assets/images/css.png'
+    },
+    {
+      name: 'git',
+      price: 23,
+      image: './assets/images/git.png'
+    },
+    {
+      name: 'html',
+      price: 34,
+      image: './assets/images/html.png'
+    },
+    {
+      name: 'js',
+      price: 3434,
+      image: './assets/images/js.png'
+    },
+    {
+    name: 'mysql',
+    price: 34,
+    image: './assets/images/mysql.png'
+    },
+    {
+      name: 'node',
+      price: 23,
+      image: './assets/images/node.png'
+    },
+    {
+      name: 'shops',
+      price: 34,
+      image: './assets/images/shops.jpg'
+    },
+    {
+      name: 'ts',
+      price: 3434,
+      image: './assets/images/ts.png'
+    }
+  ]
 }
